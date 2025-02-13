3115836010c0162fab5268d5ad71979286643518
MichelPianaro <teconhecer27@gmail.com>
2025-02-13 15:48:56 -0300

    resivando o codigo

--- src/exercicio.js
+++ src/exercicio.js
@@ -1,24 +1,10 @@
 "use strict";
-const carros = [
-    {
-        ano: 2022,
-        valor: 85000,
-        modelo: 'Gol'
-    },
-    {
-        ano: 2024,
-        valor: 120000,
-        modelo: 'Fusca'
-    }
-];
-class Saudacao {
-    dizOla(ano) {
-        return `Confira os detalhes dos carros com o ano de ${ano}:`;
-    }
+function multiplicar(a, b) {
+    return a * b;
 }
-carros.forEach((carro) => {
-    const saudacao = new Saudacao();
-    console.log(saudacao.dizOla(carro.ano));
-    console.log(`Modelo: ${carro.modelo || 'Desconhecido'}`);
-    console.log(`O valor do veículo é R$ ${carro.valor.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}.`);
-});
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+console.log(multiplicar(5, 3)); // 15
+console.log(saudacao("Michael")); // Olá Michael
